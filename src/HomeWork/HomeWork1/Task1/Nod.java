c577c7b16669e4982990123daddc8516cf1bbd5d
VakulenkoAleksey <89811702696@mail.ru>
2020-03-10 23:16:54 +0300

    Test from console

--- src/HomeWork/HomeWork1/Task1/Nod.java
+++ src/HomeWork/HomeWork1/Task1/Nod.java
@@ -5,8 +5,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-
-
 public class Nod {
     public static void main(String[] args) throws IOException, InterruptedException {
         InputStreamReader isr = new InputStreamReader(System.in);
